be60482487004d797904423403716becca074b32
M-AamirBakhtiar <aamirbakhtiar.ab@gmail.com>
2020-10-09 13:30:18 +0500

    added update, and delete

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.json());
 app.use(express.static('build'));
+app.use(express.json());
 app.use(cors());
 morgan.token('data', (req, res) => {
   return JSON.stringify(req.body);
@@ -42,58 +42,100 @@ app.use(
 //   },
 // ];
 
-app.get('/api/persons', async (req, res, next) => {
-  const persons = await Person.find({});
-
-  res.status(200).json(persons);
+app.get('/api/persons', (req, res, next) => {
+  Person.find({})
+    .then((persons) => {
+      res.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
-app.get('/api/persons/:id', async (req, res, next) => {
-  const person = await Person.findById(req.params.id);
-
-  res.status(200).json(person);
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.post('/api/persons', async (req, res, next) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body;
 
-  const person = await Person.findOne({ name: body.name });
-
   if (!body.name || !body.number) {
-    return res.status(400).json({
-      error: 'name or number is missing',
-    });
-  } else if (person) {
-    return res.status(400).json({
-      error: 'name must be unique',
-    });
-  } else {
-    const person = new Person({
-      name: body.name,
-      number: body.number,
-    });
-
-    const savedPerson = await person.save();
-
-    res.status(201).json(savedPerson);
+    return res.status(400).send({ error: 'name or number is missing' });
   }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  person
+    .save()
+    .then((savedPerson) => {
+      res.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-app.delete('/api/persons/:id', async (req, res, next) => {
-  const result = await Person.findByIdAndDelete(req.params.id);
-  console.log(result);
-  res.status(204).end();
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.get('/info', (req, res, next) => {
-  const message = `Phonebook has info for ${persons.length} people`;
-  const date = new Date().toLocaleDateString();
-  const time = new Date().toLocaleTimeString();
-  res.send(`
-  <h3>${message}</h3>
-  <p>${date} ${time}</p>
-  `);
+  Person.find({})
+    .then((persons) => {
+      const message = `Phonebook has info for ${persons.length} people`;
+      const date = new Date().toLocaleDateString();
+      const time = new Date().toLocaleTimeString();
+      res.send(`
+    <h3>${message}</h3>
+    <p>${date} ${time}</p>
+    `);
+    })
+    .catch((error) => next(error));
 });
+
+const unknownEndpoint = (req, res) => {
+  return res.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (err, req, res, next) => {
+  console.log(err.message);
+
+  if (err.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(err);
+};
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
